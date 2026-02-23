eacb7d55771b816047eac5f0c3773e1f4d2a9c91
Hercules <herculesprogramador911@gmail.com>
2026-02-23 19:08:45 -0300

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,30 @@
+import { Ionicons as IonIcons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+export default function TabsLayout() {
+ return (   
+   <Tabs
+    screenOptions={{
+    }}
+   >
+    <Tabs.Screen
+    name='index'
+    options={{
+        title: 'Todos',
+        tabBarIcon: ({ color, size }) => (
+            <IonIcons name='list' size={size} color={color} />
+        )
+    }}/>
+    
+    <Tabs.Screen
+    name='settings'
+    options={{
+        title: 'settings',
+        tabBarIcon: ({ color, size }) => (
+            <IonIcons name='settings-outline' size={size} color={color} />
+        )
+    }}/>
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
