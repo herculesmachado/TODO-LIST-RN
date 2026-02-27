79aa4bafebc5f71d62cb2a3cce5f6fb98cccc0f1
Hercules <herculesprogramador911@gmail.com>
2026-02-26 22:52:31 -0300

    Criando o nosso banco de dados e conexões CRUD

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,17 @@
 import useTheme from "@/hooks/useTheme";
 import { Text, TouchableOpacity, View } from "react-native";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Index() {
+
+  const todos = useQuery(api.todos.getTodos)
+
+  const addTodo = useMutation(api.todos.addTodo)
+
+  const deleteTodo = useMutation(api.todos.deleteTodo)
+  
+
   const {toggleDarkMode} = useTheme();
   return (
     <View
@@ -11,8 +21,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <TouchableOpacity onPress={toggleDarkMode}>
+      <TouchableOpacity onPress={() => addTodo({text: "02"})}>
       <Text>Salvefowd</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => deleteTodo({id: todos?.[0]?._id!})}>
+      <Text>delete</Text>
       </TouchableOpacity>
     </View>
   );
---
+++ convex/todos.ts
@@ -0,0 +1,72 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+// pegando todos os dados
+export const getTodos = query({
+    handler: async (ctx) => {
+        const todos = await ctx.db.query("todos").order("desc").collect()
+        return todos;
+    }
+})
+
+
+// adicionando
+export const addTodo = mutation({
+    args: {text:v.string()},
+    handler: async (ctx, args) => {
+        const newTodos = await ctx.db.insert("todos", {
+            text: args.text,
+            isCompleted: false,
+        })
+
+        return newTodos;
+    }
+})
+
+// editando
+export const toggleTodo = mutation({
+    args: {id:v.id("todos")},
+    handler:async (ctx, args) => {
+        const todo = await ctx.db.get(args.id)
+
+        if(!todo) throw new ConvexError("Todo not found");
+
+        await ctx.db.patch(args.id, {
+            isCompleted: !todo.isCompleted,
+
+        })
+    }
+})
+
+// deletando
+export const deleteTodo = mutation({
+    args: {id:v.id("todos")},
+    handler: async (ctx, args) => {
+        await ctx.db.delete(args.id);
+    }
+})
+
+// subindo a edição do texto
+export const updateTodo = mutation({
+    args: {id:v.id("todos"),
+        text:v.string()
+    },
+    handler: async (ctx, args) => {
+        await ctx.db.patch(args.id, {
+            text: args.text,
+        })
+    }
+})
+
+export const clearAllTodos = mutation({
+    handler: async (ctx) => {
+        const todos = await ctx.db.query("todos").collect();
+
+        // deleta todos
+        for (const todo of todos) {
+            await ctx.db.delete(todo._id);
+        }
+
+        return {deleteTodos: todos.length}
+    }
+})
